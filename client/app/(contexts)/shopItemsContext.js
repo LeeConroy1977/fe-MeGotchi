f357a3e123c54b5f5c7c1573bf18d97980f6898b
LeeConroy1977 <leejconroy@gmail.com>
2024-05-28 20:28:07 +0100

    shopItem context added to home page and Megotchis conditionally rendered

--- client/app/(contexts)/shopItemsContext.js
+++ client/app/(contexts)/shopItemsContext.js
@@ -9,8 +9,8 @@ const shopItemArr = [
     purchasedMsg: `You have freed Shima and returned her back to the Megotchi tribe`,
     celebrationMsg: `We Can't thank you enough for returning our friend Shima to us. She is the heart and soul of our village. We wish you luck in completing your goals...`,
     price: 20,
-    available: true,
-    purchased: false,
+    available: false,
+    purchased: true,
   },
   {
     id: 2,
@@ -20,7 +20,7 @@ const shopItemArr = [
     purchasedMsg: `You have rescude Noya and returned him back to the Megotchi tribe`,
     celebrationMsg: `How can we ever thank you for rescuing Noya from the wicked MeGotchi catcher Yamauba. Please try and complete some more goals so all our friends can be free...`,
     price: 20,
-    available: true,
+    available: false,
     purchased: false,
   },
   {
@@ -31,8 +31,8 @@ const shopItemArr = [
     purchasedMsg: `You have saved Mashimo and returned him back to the Megotchi tribe`,
     celebrationMsg: `You will be forever in our hearts for setting free the wise Mashimo. Now our tribe will be guided with great wisdom once again. Good luck with completing the rest of your goals...`,
     price: 30,
-    available: true,
-    purchased: false,
+    available: false,
+    purchased: true,
   },
   {
     id: 4,
@@ -42,7 +42,7 @@ const shopItemArr = [
     purchasedMsg: `You have set free Okuma and returned her back to the MeGotchi tribe`,
     celebrationMsg: `The tribe Will always love you for this kind act. The village was so quiet and well behaved without Okuma around. You are doing great. Keep reaching your goals... `,
     price: 40,
-    available: true,
+    available: false,
     purchased: false,
   },
 ];
