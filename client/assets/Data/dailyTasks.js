6c146e3b4e42fc450e076f3014e484b0b50eb133
Joseph Comyn <joecomyn@hotmail.com>
2024-05-29 17:22:18 +0100

    Will shorten task length to better fit format of task boxes

--- client/assets/Data/dailyTasks.js
+++ client/assets/Data/dailyTasks.js
@@ -2,26 +2,26 @@
 const dailyTasks = {
   setHappy: [
       {
-        title: "Do that thing you've been putting off",
-        body: "Lets get rid of that worry in the back of your mind, and tick off that thing we've been putting off for ages",
+        title: "Do the thing you've been putting off",
+        body: "Lets get rid of that worry in the back of your mind.",
         iconUrl: "../../assets/images/food_icon.svg",
         message: "Wow, you're on a roll today. No more worrying about that thing for us!",
       },
       {
-        title: "Enjoy your hobby/ pick up a new one",
-        body: "Hobbies are important to keep us feeling good, we should always set aside time for them. I'm quite partial to crocheting myself.",
+        title: "Enjoy your hobby",
+        body: "Hobbies are important to keep us feeling good.",
         iconUrl: "../../assets/images/food_icon.svg",
         message: "Nice, you deserve time to yourself, I'm so proud of you.",
       },
       {
         title: "Take a hot shower",
-        body: "Lets treat ourselves to a nice relaxing shower and wash all our worries away.",
+        body: "Lets have a nice relaxing shower and wash all our worries away.",
         iconUrl: "../../assets/images/food_icon.svg",
         message: "It's nice to keep clean and wash hard days away, well done.",
       },
       {
-        title: "Do a sport you enjoy/ pick up a new one",
-        body: "Sports are nice ways to stay healthy and bond with our friends, or maybe make new ones. Are there any local groups we could join?",
+        title: "Do a sport you enjoy",
+        body: "Sports are nice ways to stay healthy and bond with our friends.",
         iconUrl: "../../assets/images/food_icon.svg",
         message: "Nice! I hope that was fun.",
       },
@@ -35,43 +35,43 @@ const dailyTasks = {
         title: "Take a 5 minute walk",
         body: "Take a nice walk somewhere and breathe in the air!",
         iconUrl: "../../assets/images/food_icon.svg",
-        message: "Good job, getting those legs moving and getting some fresh air. Talk about double whammy!",
+        message: "Good job, getting those legs moving and getting some fresh air.",
       }
     ],
   setNeutral: [
     {
       title: "Journal your day",
-      body: "Journaling is a great way to explore your thoughts and could be useful when speaking to others about your feelings later.",
+      body: "Journaling is a great way to explore your thoughts.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "Writing your thoughts down can be cathartic, I hope you feel better.",
     },
     {
       title: "See the sun",
-      body: "Vitamin D is an important thing we sadly dont get enough of, especially in colder climates. It's important to soak up that sun every chance we get.",
+      body: "It's important to soak up that sun every chance we get.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "It's crazy what a bit of sun can do right? Are we plants?",
     },
     {
       title: "Stock up the fridge",
-      body: "Having food to fuel our bodies is an important part of keeping us going. Plus it helps with the midnight munchies.",
+      body: "Having food to fuel our bodies is an important part of keeping us going.",
       iconUrl: "../../assets/images/food_icon.svg",
-      message: "Thats better, how are were we gonna keep you going with no fuel? Good job",
+      message: "Thats better, how are were we gonna keep you going with no fuel?",
     },
     {
       title: "Call and vent to someone",
-      body: "Sharing feelings with someone makes the load easier for both people. We're here to lift eachother up after all, call a loved one or a hotline near you and share your feelings.",
+      body: "Sharing feelings with someone makes the load easier for both people.",
       iconUrl: "../../assets/images/food_icon.svg",
-      message: "People care about you, never be afraid to reach out, you'd be suprised who was waiting for you to call!",
+      message: "People care about you, never be afraid to reach out.",
     },
     {
       title: "Take a 5 minute walk",
       body: "Take a nice walk somewhere and breathe in the air!",
       iconUrl: "../../assets/images/food_icon.svg",
-      message: "Good job, getting those legs moving and getting some fresh air. Talk about double whammy!",
+      message: "Good job, getting those legs moving and getting some fresh air.",
     },
     {
       title: "Cook a meal for yourself",
-      body: "Cooking can be a fun and rewarding skill, creating something wonderful from ingredients. Maybe even invite someone over to experience your creation.",
+      body: "Cooking can be a fun and rewarding skill.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "Look at you master-chef, I hope it was tasty and nutritious.",
     }
@@ -81,13 +81,13 @@ const dailyTasks = {
       title: "Take a 5 minute walk",
       body: "Take a nice walk somewhere and breathe in the air!",
       iconUrl: "../../assets/images/food_icon.svg",
-      message: "Good job, getting those legs moving and getting some fresh air. Talk about double whammy!",
+      message: "Good job, getting those legs moving and getting some fresh air.",
     },
     {
       title: "Eat something",
-      body: "It can be something small, but getting something in my belly always makes me feel better, and I'm sure it'll make you feel better too!",
+      body: "Getting something in my belly always makes me feel better.",
       iconUrl: "../../assets/images/food_icon.svg",
-      message: "It can be hard to tackle the day without a full belly. I hope you're feeling a bit better.",
+      message: "It can be hard to tackle the day without a full belly.",
     },
     {
       title: "Take a hot shower",
@@ -97,19 +97,19 @@ const dailyTasks = {
     },
     {
       title: "Drink some water",
-      body: "We're around 70% water, lets keep it that way. Jokes aside we need water and it's easy to forget we need 2 litres a day to stay nice and healthy.",
+      body: "We're around 70% water, lets keep it that way.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "Keeping hydrated is important, it also helps clear skin!",
     },
     {
       title: "See the sun",
-      body: "Vitamin D is an important thing we sadly dont get enough of, especially in colder climates. It's important to soak up that sun every chance we get.",
+      body: "It's important to soak up that sun every chance we get.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "It's crazy what a bit of sun can do right? Are we plants?",
     },
     {
-      title: "Write down one thing you're thankful for",
-      body: "Reminding yourself of things that you are thankful for can be a good way of improving your mood and reminding yourself and things to look forward to",
+      title: "Write something you're thankful for",
+      body: "Remind yourself of things that you are thankful for.",
       iconUrl: "../../assets/images/food_icon.svg",
       message: "It sounds a bit generic, but heres to a glass half full!",
     }
